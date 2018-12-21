5c8d35ec21024df9c9a33fddcf9d11fc878ac866
Sangwook Cho <korean.crossfitter@gmail.com>
2018-12-21 23:26:21 +0900

    Add pug, layouts, partials

--- app.js
+++ app.js
@@ -8,16 +8,24 @@ import userRouter from './routers/userRouter'
 import videoRouter from './routers/videoRouter'
 import globalRouter from './routers/globalRouter'
 
+import { localsMiddleware } from './middlewares'
+
+import routes from './routes'
+
 const app = express()
 
+app.set('view engine', 'pug')
+
+app.use(helmet())
 app.use(cookieParser())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
-app.use(helmet())
 app.use(morgan('dev'))
 
-app.use('/', globalRouter)
-app.use('/user', userRouter)
-app.use('/video', videoRouter)
+app.use(localsMiddleware)
+
+app.use(routes.home, globalRouter)
+app.use(routes.users, userRouter)
+app.use(routes.videos, videoRouter)
 
 export default app
--- routers/videoRouter.js
+++ routers/videoRouter.js
@@ -1,5 +1,20 @@
 import express from 'express'
 
+import routes from '../routes'
+import {
+  videos,
+  upload,
+  editVideo,
+  videoDetail,
+  deleteVideo
+} from '../controllers/videoController'
+
 const videoRouter = express.Router()
+
+videoRouter.get('/', videos)
+videoRouter.get(routes.upload, upload)
+videoRouter.get(routes.videoDetail, videoDetail)
+videoRouter.get(routes.editVideo, editVideo)
+videoRouter.get(routes.deleteVideo, deleteVideo)
 
 export default videoRouter
